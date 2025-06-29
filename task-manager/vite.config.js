606f8c069b7e72035f6ac419f69870b1ae57ce40
Bella-winter <polinmwangi6115@gmail.com>
2025-06-29 18:19:32 +0300

    Configure GitHub Pages deployment

--- task-manager/vite.config.js
+++ task-manager/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/week-3-react-js-assignment-Bella-winter/",
   plugins: [
     react(),
     tailwindcss()]
